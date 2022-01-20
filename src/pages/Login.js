39bbdf283e6c166a2154d2ff63bd7b311e15b792
Alex <alexbachurin91@gmail.com>
2022-01-20 18:34:41 +0300

    login page complete

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import styled from 'styled-components';
 const Login = () => {
-    return <h2>Login Page</h2>;
+    return <Wrapper>
+        <div className="container">
+            <img src="https://res.cloudinary.com/dljezd6qv/image/upload/v1642692791/login-image-impr.jpg" alt="login_image" />
+            <h1>Github User</h1>
+            <button className="btn">Log In/Sign In</button>
+        </div>
+    </Wrapper>;
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+    border-radius: 20px;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
 
 export default Login;
